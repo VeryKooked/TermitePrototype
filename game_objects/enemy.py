3f9cd3ece3ba77b18d99d8205be95e99611e8ca7
VeryKooked <ethanwjc@hotmail.com>
2025-03-22 17:01:51 +0800

    _

--- game_objects/enemy.py
+++ game_objects/enemy.py
@@ -1,4 +1,4 @@
-# eenemy
+# enemy.py
 
 import pygame
 from game_object import GameObject
@@ -6,13 +6,13 @@ from game_object import GameObject
 class Enemy(GameObject):
     def __init__(self, x, y):
         super().__init__(x, y)
-        self.width = 40
-        self.height = 40
+        self.width = 50
+        self.height = 50
         self.hitbox = pygame.Rect(x, y, self.width, self.height)
 
     def update(self):
-        # Logic for enemy behavior can go here
+        # You can add enemy behavior here
         pass
 
     def draw(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0), self.hitbox)  # Draw a red rectangle for the enemy
+        self.hitbox = pygame.draw.rect(surface, (255, 0, 0), (self.x, self.y, self.width, self.height))
